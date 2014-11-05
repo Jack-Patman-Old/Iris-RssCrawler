527363a7a2398eadb642a011ddc67e1e1007c36d
Jack Patman <Jackpatman@Hotmail.co.uk>
2014-11-05 10:14:21 +0000

    Added check for duplicate articles
    Added IsDuplicateArticle(String url) method to avoid archiving the same
    article - check is based on url, not headline.
    
    Refactored out most database functionality to new DbAccessor class.

---
+++ src/com/iris/rssreader/db/DbAccessor.java
@@ -0,0 +1,161 @@
+package com.iris.rssreader.db;
+
+import java.net.MalformedURLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.iris.rsseader.db.properties.PropertiesAccessor;
+import com.iris.rssreader.feed.Article;
+import com.iris.rssreader.feed.RssFeed;
+
+public class DbAccessor
+{
+	private Properties prop;
+	private Connection conn;
+	
+	public DbAccessor()
+	{
+			PropertiesAccessor accessor = new PropertiesAccessor();
+			prop = accessor.GetProperties();
+
+			try
+			{
+				Class.forName("org.postgresql.Driver");
+				conn = DriverManager.getConnection(prop.getProperty("conn"), prop);
+			}
+			catch (SQLException | ClassNotFoundException e)
+			{
+				e.printStackTrace();
+			}	
+	}
+	
+	public void WriteArticlesToDb(List<Article> articles)
+	{
+		for (Article article : articles)
+		{
+
+			Statement statement;
+			String sql = null;
+
+			try
+			{
+				if (!ArticleIsDuplicate(article.getUrl()))
+				{
+					int urlId = WriteUrlToDb(article.getUrl().toString());
+
+					statement = conn.createStatement();
+					sql = "INSERT INTO \"UnprocessedArticles\" (\"FeedId\",\"Headline\",\"Description\",\"PublicationDate\",\"UrlId\") "
+							+ "VALUES ("
+							+ Integer.toString(article.getFeedId())
+							+ ","
+							+ "'"
+							+ article.getHeadline()
+							+ "',"
+							+ "'"
+							+ article.getDescription()
+							+ "',"
+							+ "'"
+							+ article.getPublicationDate() + "'," + urlId + ")";
+					statement.executeUpdate(sql);
+				}
+			}
+			catch (Exception e)
+			{
+				System.out.println("Error encountered :" + e
+						+ " executing statement :" + sql);
+			}
+		}
+	}
+	
+	public List<RssFeed> LoadFeeds()
+	{
+		List<RssFeed> feeds = new ArrayList<RssFeed>();
+
+		try
+		{
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM \"RSSFeeds\"");
+			while (rs.next())
+			{
+				int id = Integer.parseInt(rs.getString(1));
+				int outletId = Integer.parseInt(rs.getString(2));
+				String feedCategory = rs.getString(3);
+				String feedUrl = rs.getString(4);
+				String dateFormat = rs.getString(5);
+
+				feeds.add(new RssFeed(id, outletId, feedCategory, feedUrl,
+						dateFormat));
+			}
+			rs.close();
+			st.close();
+		}
+		catch (SQLException | MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+
+		return feeds;
+	}
+	
+	private int WriteUrlToDb(String url)
+	{
+		Statement statement;
+		String sql = null;
+		try
+		{
+			statement = conn.createStatement();
+			sql = "INSERT INTO \"UnprocessedArticleUrls\" (\"Url\")"
+					+ "VALUES ('" + url + "')";
+
+			statement.execute(sql, Statement.RETURN_GENERATED_KEYS);
+			ResultSet keyset = statement.getGeneratedKeys();
+			if (keyset.next())
+			{
+				// Return the generated index.
+				return (keyset.getInt(1));
+			}
+		}
+		catch (SQLException e)
+		{
+			System.out
+					.println("Exception encountered writing url to database, "
+							+ "statement was " + sql + " Exception was " + e);
+		}
+
+		return 0;
+	}
+
+	/* Checks if the url already exists in database to avoid duplicate
+	 *  archived articles. Url used as same headline may be used by different 
+	 *  news outlets */ 
+	private boolean ArticleIsDuplicate(String url)
+	{
+		String sql = null;
+
+		try
+		{
+			Statement statement = conn.createStatement();
+			sql = "SELECT * FROM \"UnprocessedArticleUrls\" WHERE \"Url\"='"
+					+ url + "' LIMIT 1";
+			ResultSet rs = statement.executeQuery(sql);
+
+			if (!rs.next())
+			{
+				return false;
+			}
+		}
+		catch (SQLException e)
+		{
+			System.out.println("Query " + sql
+					+ " failed, exception encountered was " + e);
+		}
+
+		return true;
+	}
+}
--- src/com/iris/rssreader/main/RssReader.java
+++ src/com/iris/rssreader/main/RssReader.java
@@ -1,15 +1,11 @@
 package com.iris.rssreader.main;
 
-import java.net.MalformedURLException;
-import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
-import com.iris.rsseader.db.properties.PropertiesAccessor;
+import com.iris.rssreader.db.DbAccessor;
 import com.iris.rssreader.feed.Article;
 import com.iris.rssreader.feed.RssFeed;
-
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
@@ -17,73 +13,24 @@ import com.sun.syndication.io.XmlReader;
 
 public class RssReader
 {
-	Properties prop;
-	static Connection dbConnection;
-
 	/**
 	 * When main method is invoked, will load RSS feed details from a database
 	 * extract the latest articles and archive them.
 	 */
 	public static void main(String[] args)
 	{
-		List<RssFeed> feeds = LoadFeeds();
+		DbAccessor db = new DbAccessor(); 
+		List<RssFeed> feeds = db.LoadFeeds();
 		List<Article> articles = ExtractArticles(feeds);
-		WriteArticlesToDb(articles);
+		db.WriteArticlesToDb(articles);
 	}
 
-	private static void WriteArticlesToDb(List<Article> articles)
-	{
-		for (Article article : articles)
-		{
-
-			Statement statement;
-			String sql = null;
-			int urlId = 0;
-
-			try
-			{
-				statement = dbConnection.createStatement();
-				sql = "INSERT INTO \"UnprocessedArticleUrls\" (\"Url\")"
-						+ "VALUES ('" + article.getUrl().toString() + "')";
-				statement.execute(sql, Statement.RETURN_GENERATED_KEYS);
-				ResultSet keyset = statement.getGeneratedKeys();
-				if (keyset.next())
-				{
-					// Retrieve the auto generated key(s).
-					urlId = keyset.getInt(1);
-				}
-
-				statement = dbConnection.createStatement();
-				sql = "INSERT INTO \"UnprocessedArticles\" (\"FeedId\",\"Headline\",\"Description\",\"PublicationDate\",\"UrlId\") "
-						+ "VALUES ("
-						+ Integer.toString(article.getFeedId())
-						+ ","
-						+ "'"
-						+ article.getHeadline()
-						+ "',"
-						+ "'"
-						+ article.getDescription()
-						+ "',"
-						+ "'"
-						+ article.getPublicationDate() + "'," + urlId + ")";
-				statement.executeUpdate(sql);
-			}
-			catch (Exception e)
-			{
-				System.out.println("Error encountered :" + e
-						+ " executing statement :" + sql);
-			}
-		}
-	}
-
-	/**
-	 * @param feeds
-	 * @return
-	 */
 	// Unchecked cast - should be safe based on api specification
 	@SuppressWarnings("unchecked")
 	private static List<Article> ExtractArticles(List<RssFeed> feeds)
 	{
+		/* Extract articles from a given Rss feed, generate list of articles 
+		ready to write to database */
 		List<Article> articles = new ArrayList<Article>();
 
 		for (RssFeed feed : feeds)
@@ -111,55 +58,4 @@ public class RssReader
 
 		return articles;
 	}
-
-	private static List<RssFeed> LoadFeeds()
-	{
-		List<RssFeed> feeds = new ArrayList<RssFeed>();
-
-		try
-		{
-			Class.forName("org.postgresql.Driver");
-			dbConnection = connectToDb();
-			Statement st = dbConnection.createStatement();
-			ResultSet rs = st.executeQuery("SELECT * FROM \"RSSFeeds\"");
-			while (rs.next())
-			{
-				int id = Integer.parseInt(rs.getString(1));
-				int outletId = Integer.parseInt(rs.getString(2));
-				String feedCategory = rs.getString(3);
-				String feedUrl = rs.getString(4);
-				String dateFormat = rs.getString(5);
-
-				feeds.add(new RssFeed(id, outletId, feedCategory, feedUrl,
-						dateFormat));
-			}
-			rs.close();
-			st.close();
-		}
-		catch (ClassNotFoundException | SQLException | MalformedURLException e)
-		{
-			e.printStackTrace();
-		}
-
-		return feeds;
-	}
-
-	private static Connection connectToDb()
-	{
-		Connection conn = null;
-		PropertiesAccessor accessor = new PropertiesAccessor();
-		Properties prop = accessor.GetProperties();
-
-		try
-		{
-			conn = DriverManager.getConnection(prop.getProperty("conn"), prop);
-		}
-		catch (SQLException e)
-		{
-			e.printStackTrace();
-		}
-
-		return conn;
-	}
-
 }
